e41909da9495bf15fdaf46cc52853495e6f67dbb
Alexnorvag <norvag@mail.ru>
2020-04-25 05:32:46 +0300

    Updates

---
+++ precache-manifest.90a374172be449a14097662402eb5e2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05ede88536f5602a589ee38371f4b5b2",
+    "url": "/covid19-usa/index.html"
+  },
+  {
+    "revision": "5906fef05c2bc23389a1",
+    "url": "/covid19-usa/static/css/main.d181fa88.chunk.css"
+  },
+  {
+    "revision": "bf49c07524f9873811b3",
+    "url": "/covid19-usa/static/js/2.5e2064c1.chunk.js"
+  },
+  {
+    "revision": "b4610c35fd5e6efa7493e375f2f8ce97",
+    "url": "/covid19-usa/static/js/2.5e2064c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5906fef05c2bc23389a1",
+    "url": "/covid19-usa/static/js/main.56d6948a.chunk.js"
+  },
+  {
+    "revision": "ebdf37e08102dba4d654",
+    "url": "/covid19-usa/static/js/runtime-main.6abefea3.js"
+  }
+]);
